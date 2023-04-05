f5d6cb529f01227c3deeaa02173f267c6fdea9ee
aljongjong <aljongjong@gmail.com>
2023-04-05 15:26:22 +0900

    API 데이터 재정렬 및 데이터 표현방식(테이블) 바꾸기

--- src/components/CheckWeather.vue
+++ src/components/CheckWeather.vue
@@ -2,50 +2,62 @@
     <div>
         <br>
         <h3>{{ weather.city.name }}</h3>
-        <!-- <table border=1 style="width:100%;">
-            <tr>
-                <th class='backslash' style='width:60px'><div>날짜</div>시간</th>
-                <td>2023. 04. 05</td>
-                <td>2023. 04. 06</td>
-                <td>2023. 04. 07</td>
-                <td>2023. 04. 08</td>
-                <td>2023. 04. 09</td>
-            </tr>
-            <tr>
-                <td>03:00</td>
-                <td>
-                </td>
-            </tr>
-            <tr>
-                <td>06:00</td>
-            </tr>
-            <tr>
-                <td>09:00</td>
-            </tr>
-            <tr>
-                <td>12:00</td>
-            </tr>
-            <tr>
-                <td>15:00</td>
-            </tr>
-            <tr>
-                <td>18:00</td>
-            </tr>
-            <tr>
-                <td>21:00</td>
-            </tr>
-            <tr>
-                <td>24:00</td>
-            </tr>
-        </table> -->
+        <table border=1 style="width:100%;">
+            <thead>
+                <tr>
+                    <th class='backslash' style='width:60px'><div>날짜</div>시간</th>
+                    <td v-for="(date, idx) in initDate" :key="idx" class="date" :class="date">
+                        {{ date }}
+                    </td>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td class="time-00">00:00</td>
+                    <td>1</td>
+                    <td>2</td>
+                    <td>3</td>
+                    <td>4</td>
+                    <td>5</td>
+                    <td>6</td>
+                </tr>
+                <tr>
+                    <td class="time-03">03:00</td>
+                    <!-- <span v-for="(list, index) in weather.list" :key="index">
+                        <td v-for="(obj, idx) in list" :key="idx">
+                            {{ obj.main.temp }}
+                        </td>
+                    </span> -->
+                    <span v-for="(list, index) in weather.list" :key="index">
+                        
+                    </span>
+                </tr>
+                <tr>
+                    <td class="time-06">06:00</td>
+                </tr>
+                <tr>
+                    <td class="time-09">09:00</td>
+                </tr>
+                <tr>
+                    <td class="time-12">12:00</td>
+                </tr>
+                <tr>
+                    <td class="time-15">15:00</td>
+                </tr>
+                <tr>
+                    <td class="time-18">18:00</td>
+                </tr>
+                <tr>
+                    <td class="time-21">21:00</td>
+                </tr>
+            </tbody>
+        </table>
 
         <div v-for="(list, index) in weather.list" :key="index">
             <span v-for="(obj, idx) in list" :key="idx">
                 <span v-if="obj.dt_txt">{{ obj.dt_txt }}</span> :: {{ obj.main.temp }} <br>
             </span>
         </div>
-
-
     </div>
 </template>
 
@@ -62,6 +74,7 @@ export default {
                 list: [],
                 city: {}
             },
+            initDate: new Set(),
         }
     },
     mounted() {
@@ -72,6 +85,10 @@ export default {
             this.weather.message = response.data.message;
             this.weather.list.push(response.data.list);
             this.weather.city = response.data.city;
+
+            response.data.list.forEach((obj) => {
+                this.initDate.add(obj.dt_txt.substr(0, 10));
+            });
         })
     },
 }
@@ -83,6 +100,7 @@ export default {
 }
 .backslash {
   background: url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg"><line x1="0" y1="0" x2="100%" y2="100%" stroke="gray" /></svg>');
+  font-size: 10px;
 }
 .slash, .backslash { text-align: left; }
 .slash div, .backslash div { text-align: right; }
